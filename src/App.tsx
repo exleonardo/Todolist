251a1ed8c020000c93fcaa2d839831a598cec069
Exleonardo <exleonardo@mail.ru>
2023-06-24 20:41:15 +0300

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ function App() {
   let [tasks, setTasks] = useState<Array<TaskType>>([
     {id: 1, title: "HTML&CSS", isDone: true},
     {id: 2, title: "JS", isDone: true},
-    {id: 3, title: "ReactJS", isDone: false}
+    {id: 3, title: "ReactJS", isDone: false},
+    {id: 4, title: "Redux", isDone: false}
   ]);
   let [filter, setFilter] = useState<FilterValuesType>("all");
 
@@ -18,11 +19,6 @@ function App() {
     let filteredTasks = tasks.filter((t) => t.id !== id);
     setTasks(filteredTasks)
   }
-  // const tasks2 = [
-  //   {id: 1, title: "Hello world", isDone: true},
-  //   {id: 2, title: "I am Happy", isDone: false},
-  //   {id: 3, title: "Yo", isDone: false}
-  // ]
 
   const changeFilter = (value: FilterValuesType) => {
     setFilter(value)
@@ -38,7 +34,6 @@ function App() {
   return (
     <div className="App">
       <Todolist changeFilter={changeFilter} title="What to learn" tasks={tasksForTodolist} removeTask={removeTask}/>
-      {/*<Todolist title="Songs" tasks={tasks2}/>*/}
     </div>
   );
 }
