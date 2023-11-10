eb0e4378dc33914d86849d7635cb4ca3702ff241
exleonardo <exleonardo@mail.ru>
2023-11-10 21:20:22 +0300

    delete rootReducer

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,6 @@
 import { tasksReducer } from "features/TodolistsList/Todolist/Task/tasks-reducer"
 import { todolistsReducer } from "features/TodolistsList/Todolist/todolists-reducer"
-import { AnyAction, combineReducers } from "redux"
+import { AnyAction } from "redux"
 import thunkMiddleWare, { ThunkAction } from "redux-thunk"
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
 
@@ -8,19 +8,14 @@ import { configureStore } from "@reduxjs/toolkit"
 import { authReducer } from "features/Login/authReducer"
 import { appReducer } from "app/app-reducer"
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
-const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-  auth: authReducer,
-})
-
-// непосредственно создаём store
 // export const store = legacy_createStore ( rootReducer , applyMiddleware ( thunkMiddleWare ) )
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    app: appReducer,
+    auth: authReducer,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleWare),
 })
 // определить автоматически тип всего объекта состояния
