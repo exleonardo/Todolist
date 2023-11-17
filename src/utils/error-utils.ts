71b6184e62084211ce908e527660aaa8d11e71ce
exleonardo <exleonardo@mail.ru>
2023-11-17 19:40:52 +0300

    add new handleServerNetworkError

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,7 +1,7 @@
 import { setAppErrorAC, setAppStatusAC } from "app/app-reducer"
 import { ResponseType } from "api/todolists-api"
 import { Dispatch } from "redux"
-import { AxiosError } from "axios"
+import { AxiosError, isAxiosError } from "axios"
 
 export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
   if (data.messages.length) {
@@ -12,12 +12,32 @@ export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatc
   dispatch(setAppStatusAC({ status: "failed" }))
 }
 
-export const handleServerNetworkError = (
+export const _handleServerNetworkError = (
   error: AxiosError<{ message: string }>,
   dispatch: Dispatch,
 ) => {
   dispatch(
     setAppErrorAC(error.message ? { error: error.message } : { error: "Some error occurred" }),
   )
+  dispatch(setAppStatusAC({ status: "failed" }))
+}
+
+export const handleServerNetworkError = (err: unknown, dispatch: Dispatch): void => {
+  let errorMessage = "Some error occurred"
+
+  // ❗Проверка на наличие axios ошибки
+  if (isAxiosError(err)) {
+    // ⏺️ err.response?.data?.message - например получение тасок с невалидной todolistId
+    // ⏺️ err?.message - например при создании таски в offline режиме
+    errorMessage = err.response?.data?.message || err?.message || errorMessage
+    // ❗ Проверка на наличие нативной ошибки
+  } else if (err instanceof Error) {
+    errorMessage = `Native error: ${err.message}`
+    // ❗Какой-то непонятный кейс
+  } else {
+    errorMessage = JSON.stringify(err)
+  }
+
+  dispatch(setAppErrorAC({ error: errorMessage }))
   dispatch(setAppStatusAC({ status: "failed" }))
 }
