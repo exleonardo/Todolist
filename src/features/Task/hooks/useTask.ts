7e5fd0ef973fa770fb3eac7fe213b012501b5066
Alexander Khomenok <exleonardo@mail.ru>
2024-03-17 10:57:55 +0300

    feat: fix import

--- src/features/Task/hooks/useTask.ts
+++ src/features/Task/hooks/useTask.ts
@@ -1,34 +1,37 @@
-import { useActions } from '@/utils/redux-utils'
-import { tasksAction } from '@/features/todolists-list'
 import { ChangeEvent, useCallback } from 'react'
+
 import { TaskStatuses } from '@/api/todolists-api'
 import { TaskPropsType } from '@/features/Task'
+import { tasksAction } from '@/features/todolists-list'
+import { useActions } from '@/utils/redux-utils'
 
 export const useTask = (props: TaskPropsType) => {
-  const { updateTask, removeTask } = useActions(tasksAction)
+  const { removeTask, updateTask } = useActions(tasksAction)
 
   const onClickHandler = () => removeTask({ taskId: props.task.id, todolistId: props.todolistId })
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const newIsDoneValue = e.currentTarget.checked
+
     updateTask({
-      taskId: props.task.id,
       domainModel: { status: newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New },
+      taskId: props.task.id,
       todolistId: props.todolistId,
     })
   }
   const onTitleChangeHandler = useCallback(
     (newValue: string) => {
       updateTask({
+        domainModel: { title: newValue },
         taskId: props.task.id,
         todolistId: props.todolistId,
-        domainModel: { title: newValue },
       })
     },
     [props.task.id, props.todolistId]
   )
+
   return {
-    onClickHandler,
     onChangeHandler,
+    onClickHandler,
     onTitleChangeHandler,
   }
 }
