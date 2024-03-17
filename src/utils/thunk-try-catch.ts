2ada9bd611d62297b02124d5bb9139e8417a83b0
Alexander Khomenok <exleonardo@mail.ru>
2024-03-17 10:59:31 +0300

    feat: fix import

--- src/utils/thunk-try-catch.ts
+++ src/utils/thunk-try-catch.ts
@@ -1,19 +1,21 @@
-import { handleServerNetworkError } from '@/utils/error-utils'
+import { BaseResponseType } from '@/api/todolists-api'
 import { appActions } from '@/common/common-actions/application-common-action'
 import { AppDispatchType, AppRootStateType } from '@/state/store'
-import { BaseResponseType } from '@/api/todolists-api'
+import { handleServerNetworkError } from '@/utils/error-utils'
 import { BaseThunkAPI } from '@reduxjs/toolkit/dist/createAsyncThunk'
 
 export const thunkTryCatch = async <T>(
-  thunkAPI: BaseThunkAPI<AppRootStateType, unknown, AppDispatchType, null | BaseResponseType>,
+  thunkAPI: BaseThunkAPI<AppRootStateType, unknown, AppDispatchType, BaseResponseType | null>,
   logic: () => Promise<T>
 ): Promise<ReturnType<typeof thunkAPI.rejectWithValue> | T> => {
   const { dispatch, rejectWithValue } = thunkAPI
+
   dispatch(appActions.setAppStatus({ status: 'loading' }))
   try {
     return await logic()
   } catch (e) {
     handleServerNetworkError(e, dispatch)
+
     return rejectWithValue(null)
   } finally {
     dispatch(appActions.setAppStatus({ status: 'idle' }))
