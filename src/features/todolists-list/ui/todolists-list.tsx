030c5e19d4a184ff54670f5d3f60abdbb73ae60c
Alexander Khomenok <exleonardo@mail.ru>
2024-03-17 22:03:12 +0300

    feat: add order feature

--- src/features/todolists-list/ui/todolists-list.tsx
+++ src/features/todolists-list/ui/todolists-list.tsx
@@ -1,39 +1,81 @@
-import React from 'react'
-
-import Grid from '@mui/material/Grid'
-import Paper from '@mui/material/Paper'
-
+import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd'
 import { Navigate } from 'react-router-dom'
-import { AddItemForm } from '@/widgets/addItem-form/ui/add-Item-form'
-import { Todolist } from '@/features/Todolist/ui/todolist'
+
+import { Todolist } from '@/features/Todolist'
 import { useTodolistList } from '@/features/todolists-list/hooks/useTodolistList'
+import { AddItemForm } from '@/widgets/addItem-form/ui/add-Item-form'
+
 import s from '../style/list-todolis.module.scss'
 
 type TodolistsListPropsType = {
   demo?: boolean
 }
-export const TodolistsList: React.FC<TodolistsListPropsType> = ({ demo = false }) => {
-  const { todolists, addTodolistCallback, tasks, isLoggedIn } = useTodolistList(demo)
+export const TodolistsList = ({ demo = false }: TodolistsListPropsType) => {
+  const { addTodolistCallback, changeOrderTodo, isLoggedIn, tasks, todolists } =
+    useTodolistList(demo)
+
   if (!isLoggedIn) {
     return <Navigate to={'/login'} />
   }
+  const onDragEnd = (result: DropResult) => {
+    const { destination, draggableId, source } = result
+
+    if (!destination) {
+      return
+    }
+    if (source.droppableId === destination.droppableId && source.index === destination.index) {
+      return
+    }
+    let putAfterItemId = null
+
+    if (todolists[todolists.length - 1] === todolists[destination.index]) {
+      putAfterItemId = todolists[destination.index].id
+    } else if (todolists[destination.index - 1] && source.index !== 0) {
+      putAfterItemId = todolists[destination.index - 1].id
+    } else if (source.index === 0) {
+      putAfterItemId = todolists[destination.index].id
+    }
+    changeOrderTodo({
+      id: draggableId,
+      putAfterItemId,
+    })
+  }
+  const grid = 8
+  const getListStyle = (isDraggingOver: boolean) => ({
+    background: isDraggingOver ? 'lightGreen' : 'lightblue',
+    display: 'flex',
+    overflow: 'auto',
+    padding: grid,
+  })
 
   return (
     <>
-      <Grid container className={s.container}>
-        <AddItemForm addItem={addTodolistCallback} />
-      </Grid>
-      <Grid container spacing={3}>
-        {todolists.map(tl => {
-          return (
-            <Grid item key={tl.id}>
-              <Paper className={s.contentTodo}>
-                <Todolist todolist={tl} tasks={tasks[tl.id]} demo={demo} />
-              </Paper>
-            </Grid>
-          )
-        })}
-      </Grid>
+      <AddItemForm addItem={addTodolistCallback} />
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable direction={'horizontal'} droppableId={'todos'}>
+          {(provided, snapshot) => (
+            <div
+              className={s.contentTodo}
+              style={getListStyle(snapshot.isDraggingOver)}
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {todolists.map((tl, index) => {
+                return (
+                  <Todolist
+                    demo={demo}
+                    index={index}
+                    key={tl.id}
+                    tasks={tasks[tl.id]}
+                    todolist={tl}
+                  />
+                )
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </>
   )
 }
