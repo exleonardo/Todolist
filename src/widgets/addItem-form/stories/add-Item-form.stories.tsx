448c610ef6b98c29207cb29294baf19519beda2b
Alexander Khomenok <exleonardo@mail.ru>
2024-03-19 15:56:03 +0300

    feat: sort linter

--- src/widgets/addItem-form/stories/add-Item-form.stories.tsx
+++ src/widgets/addItem-form/stories/add-Item-form.stories.tsx
@@ -1,18 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { AddItemForm } from '@/widgets/addItem-form/ui/add-Item-form'
+
 import { ReduxStoreProviderDecorator } from '@/app/decorators/redux-store-decorator'
+import { AddItemForm } from '@/widgets/addItem-form/ui/add-Item-form'
 
 const meta: Meta<typeof AddItemForm> = {
-  title: 'AddItemForm',
+  argTypes: {
+    addItem: { action: 'clicked', description: 'Button clicked inside form' },
+  },
   component: AddItemForm,
+  decorators: [ReduxStoreProviderDecorator],
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
-  decorators: [ReduxStoreProviderDecorator],
-  argTypes: {
-    addItem: { action: 'clicked', description: 'Button clicked inside form' },
-  },
+  title: 'AddItemForm',
 }
 
 export default meta
