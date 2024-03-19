f7a50caa7a781e6e32ab3e504678b33b790f78dc
Alexander Khomenok <exleonardo@mail.ru>
2024-03-19 15:47:07 +0300

    feat: refactor Task

--- src/features/Task/ui/Task.tsx
+++ src/features/Task/ui/Task.tsx
@@ -1,31 +1,56 @@
-import Checkbox from '@mui/material/Checkbox/Checkbox'
-import React from 'react'
-import { EditableSpan } from '@/widgets/editable-span/ui/editable-span'
+import React, { forwardRef } from 'react'
+import { Draggable } from 'react-beautiful-dnd'
 
-import { IconButton } from '@mui/material'
-import { Delete } from '@mui/icons-material'
 import { TaskStatuses, TaskType } from '@/api/todolists-api'
-import s from '../style/task.module.scss'
 import { useTask } from '@/features/Task/hooks/useTask'
+import { EditableSpan } from '@/widgets/editable-span/ui/editable-span'
+import { Delete } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
+
+import s from '../style/task.module.scss'
 
 export type TaskPropsType = {
+  index: number
   task: TaskType
   todolistId: string
 }
-export const Task = React.memo((props: TaskPropsType) => {
-  const { onTitleChangeHandler, onClickHandler, onChangeHandler } = useTask(props)
-  return (
-    <div key={props.task.id} className={props.task.status === TaskStatuses.Completed ? s.task : ''}>
-      <Checkbox
-        checked={props.task.status === TaskStatuses.Completed}
-        color="primary"
-        onChange={onChangeHandler}
-      />
+export const Task = React.memo(
+  forwardRef<any, TaskPropsType>(({ index, task, todolistId }: TaskPropsType) => {
+    const { onChangeHandler, onClickHandler, onTitleChangeHandler } = useTask({
+      index,
+      task,
+      todolistId,
+    })
 
-      <EditableSpan value={props.task.title} onChange={onTitleChangeHandler} />
-      <IconButton className={s.iconButtonDelete} onClick={onClickHandler}>
-        <Delete fontSize={'small'} />
-      </IconButton>
-    </div>
-  )
-})
+    return (
+      <Draggable draggableId={task.id} index={index} key={task.id}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.dragHandleProps}
+            {...provided.draggableProps}
+            className={snapshot.isDragging ? s.testTask : s.task}
+          >
+            <label className={s.container}>
+              <input
+                checked={task.status === TaskStatuses.Completed}
+                onChange={onChangeHandler}
+                type={'checkbox'}
+              />
+              <div className={s.checkmark}></div>
+            </label>
+
+            <EditableSpan
+              className={task.status === TaskStatuses.Completed ? '' : ''}
+              onChange={onTitleChangeHandler}
+              value={task.title}
+            />
+            <IconButton className={s.iconButtonDelete} onClick={onClickHandler}>
+              <Delete color={'warning'} fontSize={'small'} />
+            </IconButton>
+          </div>
+        )}
+      </Draggable>
+    )
+  })
+)
