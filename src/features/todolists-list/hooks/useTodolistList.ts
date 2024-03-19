888202a07ee82b20040824ad46ebd6a7607e3058
Alexander Khomenok <exleonardo@mail.ru>
2024-03-19 15:45:05 +0300

    feat: add onDragEnd

--- src/features/todolists-list/hooks/useTodolistList.ts
+++ src/features/todolists-list/hooks/useTodolistList.ts
@@ -1,4 +1,5 @@
 import { useCallback, useEffect } from 'react'
+import { DropResult } from 'react-beautiful-dnd'
 
 import { selectTasks, selectorTodolists } from '@/app/selectors/app-selector'
 import { todolistsActions } from '@/features/todolists-list'
@@ -11,7 +12,7 @@ export const useTodolistList = (demo: boolean = false) => {
   const tasks = useAppSelector(selectTasks)
   const isLoggedIn = useAppSelector(selectIsLoggedIn)
 
-  const { addTodolist, fetchTodolist } = useActions(todolistsActions)
+  const { addTodolist, changeOrderTodo, fetchTodolist } = useActions(todolistsActions)
 
   useEffect(() => {
     if (demo || !isLoggedIn) {
@@ -23,9 +24,42 @@ export const useTodolistList = (demo: boolean = false) => {
     return addTodolist(title).unwrap()
   }, [])
 
+  const onDragEnd = (result: DropResult) => {
+    const { destination, draggableId, source } = result
+
+    if (!destination) {
+      return
+    }
+    if (source.droppableId === destination.droppableId && source.index === destination.index) {
+      return
+    }
+    let putAfterItemId = null
+
+    if (todolists[todolists.length - 1] === todolists[destination.index]) {
+      putAfterItemId = todolists[destination.index].id
+    } else if (todolists[destination.index - 1] && source.index !== 0) {
+      putAfterItemId = todolists[destination.index - 1].id
+    } else if (source.index === 0) {
+      putAfterItemId = todolists[destination.index].id
+    }
+    const reorderedStore = [...todolists]
+    const sourceIndex = source.index
+    const destinationIndex = destination.index
+    const [removedStore] = reorderedStore.splice(sourceIndex, 1)
+
+    reorderedStore.splice(destinationIndex, 0, removedStore)
+    changeOrderTodo({
+      id: draggableId,
+      putAfterItemId,
+      reorderedStore,
+    })
+  }
+
   return {
     addTodolistCallback,
+    changeOrderTodo,
     isLoggedIn,
+    onDragEnd,
     tasks,
     todolists,
   }
