e97ed1237ba4d75b6d9ccb8cf9c5327ce00b4ddc
Alexander Khomenok <exleonardo@mail.ru>
2024-03-19 15:57:00 +0300

    feat: fix config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,15 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  overrides: [
+    {
+      files: ["**/*.stories.tsx"],
+      rules: {
+        "react-hooks/rules-of-hooks": "off",
+        "no-console": "off"
+      }
+    }
+  ],
+  extends: ["@it-incubator/eslint-config", "plugin:storybook/recommended"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
     "@typescript-eslint/no-explicit-any": "off"
-  },
+  }
 }
