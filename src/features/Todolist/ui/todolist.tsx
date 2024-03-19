ec5dce83b230646c6d290334e9808b6a3f85efb8
Alexander Khomenok <exleonardo@mail.ru>
2024-03-19 15:48:36 +0300

    feat: add DragDropContext

--- src/features/Todolist/ui/todolist.tsx
+++ src/features/Todolist/ui/todolist.tsx
@@ -1,67 +1,110 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+
+import { TaskType } from '@/api/todolists-api'
+import { buttonTodo } from '@/common/button-todo/button-todo'
+import { Task } from '@/features/Task/ui/Task'
+import { useTodolist } from '@/features/Todolist/hooks/useTodolist'
+import { FilterButton } from '@/features/filter-button/ui/filter-button'
+import { TodolistDomainType } from '@/redux/todolists-reducer'
+import { formatDate } from '@/utils/format-date'
+import { formatToTime } from '@/utils/format-time'
+import { getDayOfWeekFromString } from '@/utils/format-week'
 import { AddItemForm } from '@/widgets/addItem-form/ui/add-Item-form'
 import { EditableSpan } from '@/widgets/editable-span/ui/editable-span'
-
-import { IconButton } from '@mui/material'
 import { Delete } from '@mui/icons-material'
-import { TaskType } from '@/api/todolists-api'
-import { TodolistDomainType } from '@/redux/todolists-reducer'
-import Paper from '@mui/material/Paper'
-import { Task } from '@/features/Task/ui/Task'
-import { buttonTodo } from '@/common/button-todo/button-todo'
-import { FilterButton } from '@/features/filter-button/ui/filter-button'
-import { useTodolist } from '@/features/Todolist/hooks/useTodolist'
+
 import s from '../style/content-todo.module.scss'
 
 type PropsType = {
-  todolist: TodolistDomainType
-  tasks: TaskType[]
   demo?: boolean
+  index: number
+  tasks: TaskType[]
+  todolist: TodolistDomainType
 }
 
-export const Todolist = React.memo(function ({ ...props }: PropsType) {
-  const {
-    tasksForTodolist,
-    removeTodolistCallback,
-    changeTodolistTitleCallback,
-    addTaskCallback,
-    onFilterButtonClickHandler,
-  } = useTodolist(props.todolist, props.tasks)
-  return (
-    <Paper className={s.contentTodo}>
-      <IconButton
-        size={'small'}
-        className={s.deleteIcon}
-        onClick={removeTodolistCallback}
-        disabled={props.todolist.entityStatus === 'loading'}
-      >
-        <Delete fontSize={'small'} />
-      </IconButton>
-      <h3>
-        <EditableSpan value={props.todolist.title} onChange={changeTodolistTitleCallback} />
-      </h3>
-      <AddItemForm addItem={addTaskCallback} />
-      <div>
-        {tasksForTodolist.map(t => (
-          <Task task={t} todolistId={props.todolist.id} key={t.id} />
-        ))}
-        {!tasksForTodolist.length && (
-          <div className={s.titleFirstTask}>Create your first task☝️</div>
-        )}
+export const Todolist = React.memo(
+  forwardRef<any, PropsType>(function ({ index, tasks, todolist, ...rest }: PropsType, ref) {
+    const {
+      addTaskCallback,
+      changeTodolistTitleCallback,
+      onDragEnd,
+      onFilterButtonClickHandler,
+      removeTodolistCallback,
+      tasksForTodolist,
+    } = useTodolist(todolist, tasks)
+
+    return (
+      <div ref={ref} {...rest}>
+        <Draggable draggableId={todolist.id} index={index} key={index}>
+          {provided => (
+            <div className={s.contentTodo} ref={provided.innerRef} {...provided.draggableProps}>
+              <button
+                className={s.deleteIcon}
+                disabled={todolist.entityStatus === 'loading'}
+                onClick={removeTodolistCallback}
+              >
+                <Delete color={'error'} fontSize={'small'} />
+              </button>
+
+              <div className={s.title} {...provided.dragHandleProps}>
+                <EditableSpan onChange={changeTodolistTitleCallback} value={todolist.title} />
+              </div>
+              <div className={s.addedDate}>
+                <p>{getDayOfWeekFromString(todolist.addedDate)}:</p>
+                <p>{formatDate(todolist.addedDate)}</p>
+                <p>{formatToTime(todolist.addedDate)}</p>
+              </div>
+
+              <div className={s.contentBlock}>
+                <div className={s.taskContent}>
+                  <AddItemForm
+                    addItem={addTaskCallback}
+                    className={s.addTask}
+                    inputTitle={'Enter title task'}
+                  />
+                  <DragDropContext onDragEnd={onDragEnd}>
+                    <Droppable direction={'vertical'} droppableId={todolist.id}>
+                      {provided => (
+                        <div
+                          className={s.taskblock}
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                        >
+                          {tasksForTodolist.map((t, index) => {
+                            return (
+                              <Task index={index} key={index} task={t} todolistId={todolist.id} />
+                            )
+                          })}
+                          {provided.placeholder}
+                        </div>
+                      )}
+                    </Droppable>
+                  </DragDropContext>
+                  {!tasksForTodolist.length && (
+                    <div className={s.titleFirstTask}>Create your first task☝️</div>
+                  )}
+
+                  <div className={s.blockButtonFilter}>
+                    {buttonTodo.map((button, index) => {
+                      return (
+                        <FilterButton
+                          buttonFilter={button.buttonFilter}
+                          callBack={onFilterButtonClickHandler}
+                          color={button.color}
+                          filter={todolist.filter}
+                          key={index}
+                          text={button.text}
+                        />
+                      )
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </Draggable>
       </div>
-      <div className={s.blockButtonFilter}>
-        {buttonTodo.map(button => {
-          return (
-            <FilterButton
-              filter={props.todolist.filter}
-              color={button.color}
-              text={button.text}
-              buttonFilter={button.buttonFilter}
-              callBack={onFilterButtonClickHandler}
-            />
-          )
-        })}
-      </div>
-    </Paper>
-  )
-})
+    )
+  })
+)
