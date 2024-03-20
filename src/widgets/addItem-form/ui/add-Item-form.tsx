7cf4c5f7ba004e96691b6cfba356d2ac5b07140e
Alexander Khomenok <exleonardo@mail.ru>
2024-03-20 23:33:37 +0300

    feat: fix ui

--- src/widgets/addItem-form/ui/add-Item-form.tsx
+++ src/widgets/addItem-form/ui/add-Item-form.tsx
@@ -7,37 +7,44 @@ import s from '../style/add-item-form.module.scss'
 
 export type AddItemFormPropsType = {
   addItem: (title: string) => Promise<any>
+  buttonStyle?: string
   buttonTitle?: string
   className?: string
   disabled?: boolean
+  inputStyle?: string
   inputTitle?: string
 }
 export const AddItemForm = memo(function ({
+  buttonStyle,
   buttonTitle = '+',
   className,
   disabled = false,
+  inputStyle,
   inputTitle = 'Enter title',
   ...props
 }: AddItemFormPropsType) {
   const { addItem, error, onChangeHandler, onKeyPressHandler, title } = useAddItemForm(props)
-  const classes = clsx(className)
-  const inputClasses = clsx(error ? s.inputError : s.input)
+  const classes = clsx(className && className, error && s.errorBlock)
+  const inputClasses = clsx(error ? s.inputError : inputStyle + ' ' + s.input)
+  const buttonClasses = clsx(s.button, buttonStyle)
 
   return (
     <div className={classes}>
-      <input
-        className={inputClasses}
-        disabled={disabled}
-        onChange={onChangeHandler}
-        onKeyPress={onKeyPressHandler}
-        placeholder={inputTitle}
-        type={'text'}
-        value={title}
-      />
+      <div style={{ display: 'flex' }}>
+        <input
+          className={inputClasses}
+          disabled={disabled}
+          onChange={onChangeHandler}
+          onKeyPress={onKeyPressHandler}
+          placeholder={inputTitle}
+          type={'text'}
+          value={title}
+        />
+        <button className={buttonClasses} disabled={disabled} onClick={addItem}>
+          <span className={s.lable}>{buttonTitle}</span>
+        </button>
+      </div>
       <div style={{ color: 'red' }}>{error}</div>
-      <button className={s.button} disabled={disabled} onClick={addItem}>
-        {buttonTitle}
-      </button>
     </div>
   )
 })
